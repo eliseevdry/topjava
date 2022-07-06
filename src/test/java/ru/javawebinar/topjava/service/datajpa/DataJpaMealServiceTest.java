6eb4da1b138992c5d0a39ef1e5bcbdea39e78574
Andrey Eliseev <eliseevdry@mail.ru>
2022-07-06 15:56:59 +0300

    Fix problems in HW05, optional-5,6, start 7

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,9 +1,21 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 
 @ActiveProfiles(profiles = Profiles.DATAJPA)
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
+    @Test
+    public void getWithUser() {
+        Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+        UserTestData.USER_MATCHER.assertMatch(actual.getUser(), UserTestData.admin);
+        MEAL_MATCHER.assertMatch(actual, adminMeal1);
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -1,10 +1,27 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
 
 
 @ActiveProfiles(profiles = Profiles.DATAJPA)
 public class DataJpaUserServiceTest extends AbstractUserServiceTest {
+    @Test
+    public void getWithMeal() {
+        User user = new User(UserTestData.user);
+        User actual = service.get(USER_ID);
+        MealTestData.MEAL_MATCHER.assertMatch(actual.getMeals().get(0), MealTestData.meal1);
+        USER_MATCHER.assertMatch(actual, user);
+    }
 }
